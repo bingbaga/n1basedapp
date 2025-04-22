8b6244011679e1b0653a7ff064fbe25d8b93eac4
sharodong <9106366+bingbaga@users.noreply.github.com>
2025-04-22 17:25:41 +0800

    Update diy-mini.sh
    兼容xray go lang to 24.x

--- diy-mini.sh
+++ diy-mini.sh
@@ -137,4 +137,4 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
